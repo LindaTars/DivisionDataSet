c0d2c0970be815c9cfc7a7632b60a7880fd0b2cf
LindaTars <lindaestre.camacho@gmail.com>
2025-11-10 10:09:27 -0600

    Update settings.py

--- dataapp/dataapp/settings.py
+++ dataapp/dataapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$r6rk6etmx!1w#*(q8uv5e)esh$&!-q)gjti3knh&(2=mn(ov7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
